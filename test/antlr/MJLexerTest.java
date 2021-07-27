9cc68e293b304e5e36cdd9f14c8d78e250ffb5f0
Diogo <>
2021-07-27 18:43:28 -0500

    First commit

---
+++ test/antlr/MJLexerTest.java
@@ -0,0 +1,264 @@
+package antlr;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.Lexer;
+import org.junit.Test;
+
+public class MJLexerTest {
+    
+    @Test
+    public void TestClassKW() throws IOException {
+        String classString = "class";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.CLASS_KW);
+    }
+    
+    @Test
+    public void TestPublicKW() throws IOException {
+        String classString = "public";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.PUBLIC_KW);
+    }
+    
+    @Test
+    public void TestStaticKW() throws IOException {
+        String classString = "static";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.STATIC_KW);
+    }
+    
+    @Test
+    public void TestVoidKW() throws IOException {
+        String classString = "void";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.VOID_KW);
+    }
+    
+    @Test
+    public void TestMainKW() throws IOException {
+        String classString = "main";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.MAIN_KW);
+    }
+    
+    @Test
+    public void TestStringKW() throws IOException {
+        String classString = "String";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.STRING_KW);
+    }
+    
+    @Test
+    public void TestIntKW() throws IOException {
+        String classString = "int";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.INT_KW);
+    }
+    
+    @Test
+    public void TestBooleanKW() throws IOException {
+        String classString = "boolean";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.BOOLEAN_KW);
+    }
+    
+    @Test
+    public void TestLParens() throws IOException {
+        String classString = "(";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.LPARENS);
+    }
+    
+    @Test
+    public void TestRParens() throws IOException {
+        String classString = ")";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.RPARENS);
+    }
+    
+    @Test
+    public void TestLBracket() throws IOException {
+        String classString = "[";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.LBRACKET);
+    }
+    
+    @Test
+    public void TestRBracket() throws IOException {
+        String classString = "]";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.RBRACKET);
+    }
+    
+    @Test
+    public void TestCurlyLBracket() throws IOException {
+        String classString = "{";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.CURLY_LBRACKET);
+    }
+    
+    @Test
+    public void TestCurlyRBracket() throws IOException {
+        String classString = "}";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.CURLY_RBRACKET);
+    }
+    
+    @Test
+    public void TestIf() throws IOException {
+        String classString = "if";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.IF);
+    }
+    
+    @Test
+    public void TestWhile() throws IOException {
+        String classString = "while";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.WHILE);
+    }
+    
+    @Test
+    public void TestPrintln() throws IOException {
+        String classString = "System.out.println";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.PRINTLN);
+    }
+    
+    @Test
+    public void TestAnd() throws IOException {
+        String classString = "&&";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.AND);
+    }
+    
+    @Test
+    public void TestNot() throws IOException {
+        String classString = "!";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.NOT);
+    }
+    
+    @Test
+    public void TestLT() throws IOException {
+        String classString = "<";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.LT);
+    }
+    
+    @Test
+    public void TestPlus() throws IOException {
+        String classString = "+";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.PLUS);
+    }
+    
+    @Test
+    public void TestMinus() throws IOException {
+        String classString = "-";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.MINUS);
+    }
+    
+    @Test
+    public void TestMult() throws IOException {
+        String classString = "*";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.MULT);
+    }
+    
+    @Test
+    public void TestDot() throws IOException {
+        String classString = ".";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.DOT);
+    }
+    
+    @Test
+    public void TestTrue() throws IOException {
+        String classString = "true";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.TRUE);
+    }
+    
+    @Test
+    public void TestFalse() throws IOException {
+        String classString = "false";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.FALSE);
+    }
+    
+    @Test
+    public void TestThis() throws IOException {
+        String classString = "this";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.THIS);
+    }
+    
+    @Test
+    public void TestNew() throws IOException {
+        String classString = "new";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.NEW);
+    }
+    
+    @Test
+    public void TestID() throws IOException {
+        String classString = "my_string";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.ID);
+    }
+    
+    @Test
+    public void TestNewLine() throws IOException {
+        String classString = "new" + "\n" + "line";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.NEW);
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.ID);
+    }
+    
+    @Test
+    public void TestComment() throws IOException {
+        String classString = "// some comment" + "\n" + "line";
+        InputStream targetStream = new ByteArrayInputStream(classString.getBytes());
+        Lexer mjLexer = new MJLexer(CharStreams.fromStream(targetStream));
+        assertEquals(mjLexer.nextToken().getType(), MJLexer.ID);
+    }
+
+}
